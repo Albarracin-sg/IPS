81a5b2472ca259c88d908aed95d4c536700b9b57
Albarracin-sg <camilinplin2024@gmail.com>
2025-04-02 20:08:42 -0500

    estructura basica de reat con las carpetas que vamos a usar

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+//import { BrowserRouter } from "react-router-dom";
+//import AppRoutes from "./routes/AppRoutes";
+
+function App() {
+  return (
+    /*<BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>*/
+    <div>
+      <h1>Welcome to the React App</h1>
+    </div>
+  );
+}
+
+export default App;
