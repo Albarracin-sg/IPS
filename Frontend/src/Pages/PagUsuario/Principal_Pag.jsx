137c99297f365b08010de9190aa3eb97b90bffa3
Albarracin-sg <camilinplin2024@gmail.com>
2025-04-03 22:53:36 -0500

    registro responsive

--- Frontend/src/Pages/PagUsuario/Principal_Pag.jsx
+++ Frontend/src/Pages/PagUsuario/Principal_Pag.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import ipsLogo from "../../assets/ips.png";
 import Card from "../../UsuarioQR/components/Principal/Card";
 
 export default function PagUsuarioPrincipal() {
     return (
-        <div className="w-full h-[100vh] bg-gradient-to-br from-blue-400 via-blue-500 to-blue-600 flex justify-center items-center relative overflow-hidden">
-            <div>
-                <Card />
-            </div>
-        </div>
+        <div className="relative min-h-screen w-screen bg-[#6ea3c7] flex justify-center items-center overflow-auto">
+			<div className="absolute top-0 left-0 m-4">
+				{/* Imagen de la IPS */}
+				<img
+					src={ipsLogo}
+					alt="IPS Logo"
+					className="w-20 h-10 md:w-30 md:h-15" // Mantiene el tamaño original
+				/>
+			</div>
+			<Card />
+		</div>
     );
 }
