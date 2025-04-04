dd885cfca8194e3c86a028cc2f6fe017f6e2ec4e
Esteban2309 <estivan2309@gmail.com>
2025-04-04 14:57:40 -0500

    nose

---
+++ Frontend/src/UsuarioQR/components/Register/FormulariosTicket/Form_General.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ipsLogo from "../../../../assets/ips.png"; // Asegúrate de que la ruta sea correcta
+import BotonNavegacion from "./BotonNavegacion";
+
+const FormularioGeneral = () => {
+    return (
+        <div className="relative min-h-screen w-screen bg-[#6ea3c7] flex justify-center items-center overflow-auto">
+            <div className="absolute top-0 left-0 m-4">
+                {/* Imagen de la IPS */}
+                <img
+                    src={ipsLogo}
+                    alt="IPS Logo"
+                    className="w-20 h-10 md:w-30 md:h-15" // Mantiene el tamaño original
+                />
+            </div>
+            <div className=" md:flex-row items-center w-full max-w-5xl p-6 rounded-lg bg-white shadow-xl">
+                <h2 className="text-center text-3xl font-semibold text-black-700 mb-6">Registro de Usuario</h2>
+                <form className="text-gray-700 flex flex-col space-y-4 w-full">
+                    <div className="flex flex-col space-y-2">
+                        <label className="font-medium text-black-700" htmlFor="nombre">
+                            Nombres Completos
+                        </label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            required
+                            className="p-3 rounded-md border border-blue-300 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-blue-50 placeholder-gray-400"
+                            placeholder="Ingresa tus nombres completos"
+                        />
+                    </div>
+                    <div className="flex justify-between">
+                        <BotonNavegacion texto={("Anterior")} />
+                        <BotonNavegacion texto={("Siguiente")} />
+                    </div>
+                </form>
+            </div>
+
+        </div>
+
+    );
+};
+
+export default FormularioGeneral;
--- Frontend/src/routes/AppRoutes.jsx
+++ Frontend/src/routes/AppRoutes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import PrincipaPage from "../pages/PagUsuario/Principal_Pag";
 import RegisterForm from "../pages/PagUsuario/Register_Pag";
-
+import FormularioGeneral from "../UsuarioQR/components/Register/FormulariosTicket/Form_General";
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<PrincipaPage />} />
-        <Route path="/registro" element={<RegisterForm />} />
+      <Route path="/registro" element={<RegisterForm />} />
+      <Route path="/login" element={<FormularioGeneral />} />
     </Routes>
   );
 }
