d149d9d360806b388d7b9df053800a7b926e46e2
Jajavier2404 <javialex2008@gmail.com>
2025-04-03 19:16:59 -0500

    Cambios

--- Frontend/src/UsuarioQR/components/Principal/Card.jsx
+++ Frontend/src/UsuarioQR/components/Principal/Card.jsx
@@ -13,7 +13,7 @@ export default function Card() {
                 <input 
                     type="text"
                     className="w-full pl-10 pr-4 py-3 border-2 border-gray-200 rounded-lg focus:outline-none focus:border-blue-500 transition-colors text-gray-700"
-                    placeholder="Buscar paciente..."
+                    placeholder="Numero de Documento"
                 />
                 <div className="absolute left-3 top-3.5 text-gray-400">
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
